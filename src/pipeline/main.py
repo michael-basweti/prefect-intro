f02a2c6d8d35ea0d90f0f345f193c750e4e04890
michael-basweti <baswetima@gmail.com>
2026-08-04 16:55:02 +0300

    run whole flow

--- src/pipeline/main.py
+++ src/pipeline/main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+import datetime
+
 import httpx
+import psycopg2
 from prefect import flow, get_run_logger, task
 
 
@@ -19,14 +22,68 @@ def retrieve_petstore_inventory(base_url: str, path: str, secure: bool):
     return inventory_stats
 
 
+# Task to clean the status date
+@task(name="clean_status_date")
+def clean_status_date(inventory_stats: dict) -> dict:
+    return {
+        "sold": int(inventory_stats.get("sold", 0))
+        + int(inventory_stats.get("sold ", 0))
+        + int(inventory_stats.get("SOLD", 0)),
+        "available": int(inventory_stats.get("available", 0))
+        + int(inventory_stats.get("avalible", 0))
+        + int(inventory_stats.get("availabl", 0))
+        + int(inventory_stats.get("available, sold", 0))
+        + int(inventory_stats.get("avaliable", 0)),
+        "pending": int(inventory_stats.get("pending", 0))
+        + int(inventory_stats.get("penidng", 0)),
+        "unavailable": int(inventory_stats.get("NOT available", 0)),
+    }
+
+
+@task(name="insert_to_db")
+def insert_to_db(
+    inventory_stats: dict,
+    db_host: str,
+    db_user: str,
+    db_password: str,
+    db_name: str,
+):
+    # Placeholder for database insertion logic
+    with psycopg2.connect(
+        host=db_host, user=db_user, password=db_password, dbname=db_name
+    ) as conn:
+        with conn.cursor() as cursor:
+            # Example insertion logic
+            cursor.execute(
+                """
+                INSERT INTO inventory_history (fetch_timestamp, sold, available, pending, unavailable)
+                VALUES (%s, %s, %s, %s, %s)
+                """,
+                (
+                    datetime.datetime.now(),
+                    inventory_stats.get("sold", 0),
+                    inventory_stats.get("available", 0),
+                    inventory_stats.get("pending", 0),
+                    inventory_stats.get("unavailable", 0),
+                ),
+            )
+            conn.commit()
+
+
 # Flow to collect petstore inventory
 @flow(name="Collect Petstore Inventory")
 def collect_petstore_inventory(
     base_url: str = "petstore.swagger.io",
     path: str = "/v2/store/inventory",
     secure: bool = True,
+    db_host: str = "localhost",
+    db_user: str = "root",
+    db_password: str = "root",
+    db_name: str = "petstore",
 ):
     inventory_stats = retrieve_petstore_inventory(base_url, path, secure)
+    inventory_stats = clean_status_date(inventory_stats)
+    insert_to_db(inventory_stats, db_host, db_user, db_password, db_name)
 
 
 def main():
